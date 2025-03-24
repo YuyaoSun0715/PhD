66cc4b258d850d096bd32068ec6742790ae183ee
YuyaoSun0715 <YUYAO001@E.NTU.EDU.SG>
2025-03-24 16:34:16 +0800

    Update maml.py

--- maml.py
+++ maml.py
@@ -1,71 +1,115 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import numpy as np
+import torch.nn.functional as F
 
+def conv_block(in_channels, out_channels, kernel_size=3, padding=1, use_maxpool=True):
+    layers = [
+        nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding),
+        nn.ReLU(inplace=True)
+    ]
+    if use_maxpool:
+        layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
 
-class MAML(nn.Module):
-    def __init__(self, dim_input=1, dim_output=1, hidden_layers=[40, 40], update_lr=0.01, meta_lr=0.001, num_updates=5):
-        super(MAML, self).__init__()
-        self.dim_input = dim_input
-        self.dim_output = dim_output
-        self.hidden_layers = hidden_layers
-        self.update_lr = update_lr
-        self.meta_lr = meta_lr
-        self.num_updates = num_updates
+class ConvNet(nn.Module):
+    def __init__(self, in_channels, num_classes, num_filters=64):
+        """
+        A simple 4-layer convolutional network.
+        """
+        super(ConvNet, self).__init__()
+        self.conv1 = conv_block(in_channels, num_filters)
+        self.conv2 = conv_block(num_filters, num_filters)
+        self.conv3 = conv_block(num_filters, num_filters)
+        self.conv4 = conv_block(num_filters, num_filters)
+        # Use adaptive pooling to ensure fixed feature dimension regardless of input size.
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(num_filters, num_classes)
 
-        self.network = self.build_network()
-        self.loss_fn = nn.MSELoss()
-        self.meta_optimizer = optim.Adam(self.network.parameters(), lr=self.meta_lr)
-
-    def build_network(self):
-        layers = []
-        input_dim = self.dim_input
-        for hidden_dim in self.hidden_layers:
-            layers.append(nn.Linear(input_dim, hidden_dim))
-            layers.append(nn.ReLU())
-            input_dim = hidden_dim
-        layers.append(nn.Linear(input_dim, self.dim_output))
-        return nn.Sequential(*layers)
-
-    def forward(self, x, weights=None):
-        if weights is None:
-            return self.network(x)
+    def forward(self, x, params=None):
+        """
+        If params is None, use the module's own parameters.
+        Otherwise, use the provided parameter dictionary (for fast adaptation).
+        """
+        if params is None:
+            x = self.conv1(x)
+            x = self.conv2(x)
+            x = self.conv3(x)
+            x = self.conv4(x)
         else:
-            out = x
-            for i in range(0, len(weights) - 2, 2):
-                out = torch.matmul(out, weights[i]) + weights[i + 1]
-                out = torch.relu(out)
-            return torch.matmul(out, weights[-2]) + weights[-1]
+            # Functional forward pass using the parameters from the dict.
+            # Note: This assumes the same architecture as defined above.
+            x = F.conv2d(x, params['conv1.0.weight'], params['conv1.0.bias'], stride=1, padding=1)
+            x = F.relu(x)
+            x = F.max_pool2d(x, 2)
 
-    def clone_weights(self):
-        return [param.clone() for param in self.network.parameters()]
+            x = F.conv2d(x, params['conv2.0.weight'], params['conv2.0.bias'], stride=1, padding=1)
+            x = F.relu(x)
+            x = F.max_pool2d(x, 2)
 
-    def task_adaptation(self, x, y):
-        fast_weights = self.clone_weights()
-        for _ in range(self.num_updates):
-            preds = self.forward(x, fast_weights)
-            loss = self.loss_fn(preds, y)
-            grads = torch.autograd.grad(loss, fast_weights, create_graph=True)
-            fast_weights = [w - self.update_lr * g for w, g in zip(fast_weights, grads)]
-        return fast_weights
+            x = F.conv2d(x, params['conv3.0.weight'], params['conv3.0.bias'], stride=1, padding=1)
+            x = F.relu(x)
+            x = F.max_pool2d(x, 2)
 
-    def meta_update(self, task_batch):
-        meta_loss = 0.0
-        for x_train, y_train, x_test, y_test in task_batch:
-            fast_weights = self.task_adaptation(x_train, y_train)
-            preds = self.forward(x_test, fast_weights)
-            meta_loss += self.loss_fn(preds, y_test)
-        self.meta_optimizer.zero_grad()
-        meta_loss.backward()
-        self.meta_optimizer.step()
-        return meta_loss.item()
+            x = F.conv2d(x, params['conv4.0.weight'], params['conv4.0.bias'], stride=1, padding=1)
+            x = F.relu(x)
+            x = F.max_pool2d(x, 2)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        if params is None:
+            logits = self.fc(x)
+        else:
+            logits = F.linear(x, params['fc.weight'], params['fc.bias'])
+        return logits
 
+class MAML:
+    def __init__(self, model, update_lr, num_updates):
+        """
+        Args:
+            model: the ConvNet to be adapted.
+            update_lr: inner-loop learning rate.
+            num_updates: number of inner gradient update steps.
+        """
+        self.model = model
+        self.update_lr = update_lr
+        self.num_updates = num_updates
+        self.loss_fn = nn.CrossEntropyLoss()
 
-# Example usage
-maml = MAML(dim_input=1, dim_output=1)
-x = torch.randn(5, 1)
-y = torch.randn(5, 1)
-task_batch = [(x, y, x, y)]  # Example dummy task batch
-meta_loss = maml.meta_update(task_batch)
-print("Meta Loss:", meta_loss)
+    def meta_loss(self, support_x, support_y, query_x, query_y):
+        meta_batch_size = support_x.size(0)
+        total_query_loss = 0
+        total_accuracy = 0
+
+        # Process each task in the meta-batch separately.
+        for i in range(meta_batch_size):
+            s_x = support_x[i]  # shape: [ways*shots, C, H, W]
+            s_y = support_y[i]  # shape: [ways*shots]
+            q_x = query_x[i]  # shape: [ways*queries, C, H, W]
+            q_y = query_y[i]  # shape: [ways*queries]
+
+            # Initialize parameters for this task.
+            params = {name: param for name, param in self.model.named_parameters()}
+
+            # Compute loss on the support set.
+            logits = self.model.forward(s_x, params)
+            loss = self.loss_fn(logits, s_y)
+
+            # Inner-loop adaptation.
+            for _ in range(self.num_updates):
+                grads = torch.autograd.grad(loss, params.values(), create_graph=True)
+                params = {name: param - self.update_lr * grad
+                          for (name, param), grad in zip(params.items(), grads)}
+                logits = self.model.forward(s_x, params)
+                loss = self.loss_fn(logits, s_y)
+
+            # Compute query loss and accuracy using the adapted parameters.
+            query_logits = self.model.forward(q_x, params)
+            q_loss = self.loss_fn(query_logits, q_y)
+            preds = torch.argmax(query_logits, dim=1)
+            task_accuracy = (preds == q_y).float().mean()
+
+            total_query_loss += q_loss
+            total_accuracy += task_accuracy
+
+        avg_query_loss = total_query_loss / meta_batch_size
+        avg_accuracy = total_accuracy / meta_batch_size
+        return avg_query_loss, avg_accuracy
